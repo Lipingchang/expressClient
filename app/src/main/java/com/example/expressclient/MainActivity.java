b339f625de382a0fe3c1763b60587e016d5c4d3e
lipc <3236568459@qq.com>
2019-05-15 20:11:59 +0800

    add navigation framgment support

--- app/src/main/java/com/example/expressclient/MainActivity.java
+++ app/src/main/java/com/example/expressclient/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.expressclient;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class MainActivity extends AppCompatActivity
+implements
+        nav_home.OnFragmentInteractionListenerNavHome,
+        nav_newSend.OnFragmentInteractionListenerNewSend,
+        nav_userCenter.OnFragmentInteractionListenerNavUserCenter
+{
 
     private TextView mTextMessage;
+    // 路由控制器
+    private NavController navController;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +30,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.nav_home:
                     mTextMessage.setText(R.string.nav_home);
+                    navController.navigate(R.id.nav_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.nav_newSend:
                     mTextMessage.setText(R.string.nav_newSend);
+                    navController.navigate(R.id.nav_newSend);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.nav_userCenter:
                     mTextMessage.setText(R.string.nav_userCenter);
+                    navController.navigate(R.id.nav_userCenter);
                     return true;
             }
             return false;
@@ -39,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        this.navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+
     }
 
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
