c8c3838455f9853b771ca6ab5f47117b53636b6a
lipc_1700 <3236568459@somechatapp.com>
2019-05-17 20:55:51 +0800

    import jar from baidusdk

--- app/src/main/java/com/example/expressclient/bean/Order.java
+++ app/src/main/java/com/example/expressclient/bean/Order.java
@@ -1,5 +1,18 @@
 package com.example.expressclient.bean;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+    int orderId;
+    String senderName;
+    String senderTelephone;
+//        "startPlace": "{\"lng\": 120.1619520000, \"lat\":30.3286630000}",
+    String goodsType;
+    float goodsWeight;
+    String receiverName;
+    String receiverTelephone;
+//        "endPlace": "{\"lat\": 30.3248220000, \"lng\":120.1500080000}",
+    int orderStatus;
+    Date createTime;
+    Date updateTime;
 }
